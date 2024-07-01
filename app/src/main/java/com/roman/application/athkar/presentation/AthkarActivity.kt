ab6ef2bd0558bd5b5c2c6a9c4cc3d9a12ac3aa2f
Abdul Rehman <abdulrehman02123@gmail.com>
2024-07-01 12:17:48 +0500

    viewpager 2 listeners

--- app/src/main/java/com/roman/application/athkar/presentation/AthkarActivity.kt
+++ app/src/main/java/com/roman/application/athkar/presentation/AthkarActivity.kt
@@ -1,22 +1,25 @@
 package com.roman.application.athkar.presentation
 
+import android.util.Log
 import android.view.LayoutInflater
 import androidx.activity.viewModels
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.roman.application.athkar.AkhtarAdapter
 import com.roman.application.athkar.domain.model.AkhtarResponse
 import com.roman.application.athkar.domain.model.Athkar
 import com.roman.application.base.BaseCompatVBActivity
 import com.roman.application.databinding.ActivityAthkarBinding
-import com.roman.application.home.domain.model.response.prayer.CurrentPrayerDetail
 import com.roman.application.util.network.ErrorResponse
 import com.roman.application.util.network.NetworkResult
 import com.roman.application.util.showToast
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class AthkarActivity : BaseCompatVBActivity<ActivityAthkarBinding>() {
 
     private val viewModel: AkhtarViewModel by viewModels()
+    private var athkars: ArrayList<Athkar?>? = null
 
     override fun setUpViewBinding(layoutInflater: LayoutInflater): ActivityAthkarBinding {
         return ActivityAthkarBinding.inflate(layoutInflater)
@@ -28,12 +31,18 @@ class AthkarActivity : BaseCompatVBActivity<ActivityAthkarBinding>() {
         mBinding?.appCompatImageView2?.setOnClickListener {
             finish()
         }
+        mBinding?.tvBack?.setOnClickListener {
+            mBinding?.recyclerView?.currentItem = mBinding?.recyclerView?.currentItem!! - 1
+        }
+        mBinding?.tvNext?.setOnClickListener {
+            mBinding?.recyclerView?.currentItem = mBinding?.recyclerView?.currentItem!! + 1
+        }
 
         viewModel.getAkhtarData()
     }
 
 
-    private fun setAdapter(list: ArrayList<Athkar?>?){
+    private fun setAdapter(list: ArrayList<Athkar?>?) {
         val adapter = AkhtarAdapter(list)
         mBinding?.recyclerView?.adapter = adapter
         mBinding?.recyclerView?.let { mBinding?.indicatorView?.setupWithViewPager(it) }
@@ -46,6 +55,7 @@ class AthkarActivity : BaseCompatVBActivity<ActivityAthkarBinding>() {
             when (it) {
                 is NetworkResult.Success -> {
                     val data = it.result as AkhtarResponse
+                    athkars = data.athkars
                     setAdapter(data.athkars)
                 }
 
@@ -59,5 +69,26 @@ class AthkarActivity : BaseCompatVBActivity<ActivityAthkarBinding>() {
                 }
             }
         }
+    }
+
+    private fun addListener() {
+        mBinding?.recyclerView?.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+            }
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                super.onPageScrollStateChanged(state)
+            }
+        })
     }
 }
